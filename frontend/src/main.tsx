47595e6ddf2971c6ff5f2f45a71afaf85e791174
Sergey <gssdsudas@gmail.com>
2024-08-12 11:26:10 +0400

    chore: frontend structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from '@/pages/routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <></>,
+  <RouterProvider router={routes} />,
 )
